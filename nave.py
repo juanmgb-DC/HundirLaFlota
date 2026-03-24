1aea3dce02c2035c3337bae67bcbcb4f56c581d3
Juan <j.martinezgonzalez-besada@danielcastelao.org>
2026-03-24 14:00:37 +0100

    Comentarios nave

--- nave.py
+++ nave.py
@@ -8,10 +8,15 @@ class Nave:
     def recibir_disparo(self):
         if self.hundido:
             return 2
+            # Si la nave está hundida devuelve 2
 
         self.vida -= 1
+            # Resta una vida por el disparo recibido
+
 
         if self.vida <= 0:
             self.hundido = True
             return 2
+            # Si no le quedan vidas , la nave está hundida y devuelve 2
         return 1
+            # Si ha recibido el disparo pero todavia conserva vidas, está tocado
